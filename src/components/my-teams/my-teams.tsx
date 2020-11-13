bbecec8b8259cae0ef4554173894248664e71683
Lucas Costa <lucasale00@hotmail.com>
2020-11-13 17:26:35 -0300

    datatable material ui

--- src/components/my-teams/my-teams.tsx
+++ src/components/my-teams/my-teams.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
 import './my-teams.scss';
+import MUIDataTable from "mui-datatables";
 
 function MyTeams() {
-  const columnsValues: Array<string> = ["B_Value", "C_Value", "A_Value"];
-  let columns: Array<JSX.Element> = [];
-  columnsValues.sort((a,b) => 0 - (a > b ? -1 : 1));
-  columnsValues.forEach(column => {
-    columns.push(<span className="td">{column}</span>);
-  });
+  const columns = [
+    {
+     name: "name",
+     label: "Name",
+     options: {
+      sort: true,
+     }
+    },
+    {
+     name: "company",
+     label: "Company",
+     options: {
+      sort: true,
+     }
+    },
+   ];
+   
+   const data = [
+    { name: "Joe James", company: "Test Corp" },
+    { name: "John Walsh", company: "Test Corp" },
+    { name: "Bob Herm", company: "Test Corp" },
+    { name: "James Houston", company: "Test Corp" },
+   ];
+   
+   const options = {
+    search: false,
+    showTitle: false,
+    toolbar: false,
+    filter: false,
+    download: false,
+    print: false,
+    viewColumns: false,
+    selection: false,
+    paging: true,
+    selectableRows: 'none',
+        onRowClick: (rowData) => {
+            console.log("RowClicked->", rowData);
+        },
+        responsive: "stacked",
+        fixedHeaderOptions: {
+            xAxis: false,
+            yAxis: false,
+        },
+   };
   return(
     <>
       <div className="my-teams">
         <div className="title">
           <span>My Teams</span>
         </div>
-
-        <div className="table">
-          <div className="thead">
-            <div className="tr">
-              {columns}
-            </div>
-          </div>
-          <div className="tbody">
-            <div className="tr">
-              <span className="td">Name</span>
-              <span className="td">description</span>
-            </div>
-          </div>
-
-        </div>
+        <MUIDataTable
+          data={data}
+          columns={columns}
+          options={options}
+        />
       </div>
     </>
   );
